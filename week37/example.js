cc7c2ed36f6ff1d4b775d66e3d899e305ef8e3fd
OIH <OIH@VIA.DK>
2017-09-18 13:59:26 +0200

    Examples of prototypical and class inheritance

--- week37/example.js
+++ week37/example.js
@@ -1,27 +1,57 @@
 // Colored point with prototypes
-point_prototype = {
+// "Instantiation"
+function Point(x, y) {
+    this.x = x
+    this.y = y
+}
+
+Point.prototype = {
     move: function(dx, dy) {
         this.x += dx
         this.y += dy
     }
 }
 
-// "Instantiation"
-function Point(x, y) {
-    var p = Object.create(point_prototype)
-    p.x = x
-    p.y = y
+// Like var p = new Point(x, y)
+function createPoint(x, y) {
+    var p = Object.create(Point.prototype)
+    Point.call(p, x, y)
+    p.constructor = Point
     return p
-}
-
-colored_point_prototype = {
-    getColor: function() { return this.color }
 }
 
 // "Inheritance"
 function ColoredPoint(x, y, color) {
-    var cp = Object.create(Point(x, y))
-    cp.color = color
-    return cp
+    Point.call(this, x, y)
+    this.color = color
 }
 
+
+ColoredPoint.prototype = {
+    getColor: function() { return this.color }
+}
+Object.setPrototypeOf(ColoredPoint.prototype, Point.prototype)
+
+// ES2015 (ES6):
+class PointClass {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+
+    move(dx, dy) {
+        this.x += dx
+        this.y += dy
+    }
+}
+
+class ColoredPointClass extends PointClass {
+    constructor(x, y, color) {
+        super(x, y)
+        this.color = color
+    }
+
+    getColor() {
+        return this.color
+    }
+}
