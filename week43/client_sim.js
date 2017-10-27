f7e273c13c4b4f3202309ab394e08668a90fc82c
Ole Ildsgaard Hougaard (OIH) | VIA <OIH@VIAUC.DK>
2017-10-27 09:56:54 +0200

    reverted to the basic version

--- week43/client_sim.js
+++ week43/client_sim.js
@@ -1,6 +1,2 @@
-const { promise: { validate, authorize, secret } } = require('./server_sim.js')
+const { callback, promise } = require('./server_sim.js')
 
-validate()
-.then (authorize)
-.then (auth => Promise.all([0,1,2].map(i => secret(i, auth))))
-.then (console.log)
